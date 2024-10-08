0c11aa8f28949e3b03aba652389ce2a4f9ea5fcc
166236uwm <166236@student.uwm.edu.pl>
2024-10-08 15:58:50 +0200

    added gitignore

--- back/obj/Debug/net8.0/Magazyn.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/Magazyn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magazyn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da16d4b26ae7caea224cbf9f7adc39e8d42ec6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39142bd280478ec688672bba188c4e7621335d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magazyn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magazyn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
